46b6fae47f6cd998bff1554edff9c73a06224acf
Prashant Singh <prashantsingh061201@gmail.com>
2022-03-09 17:06:36 +0530

    Time: 753 ms (8.81%), Space: 78.6 MB (8.89%) - LeetHub

--- 18-4sum/18-4sum.cpp
+++ 18-4sum/18-4sum.cpp
@@ -1,9 +1,26 @@
+struct hashFunction 
+{
+  size_t operator()(const vector<int> 
+                    &myVector) const 
+  {
+    std::hash<int> hasher;
+    size_t answer = 0;
+      
+    for (int i : myVector) 
+    {
+      answer ^= hasher(i) + 0x9e3779b9 + 
+                (answer << 6) + (answer >> 2);
+    }
+    return answer;
+  }
+};
+  
 class Solution {
 public:
     vector<vector<int>> fourSum(vector<int>& nums, int target) {
         sort(nums.begin(),nums.end());
         int n=nums.size(),start=0,end,sum1=target;
-        set<vector<int>> ans; 
+        unordered_set<vector<int>,hashFunction> ans; 
         vector<vector<int>> res; 
         for(int i=0;i<n;i++){
             for(int j=i+1;j<n;j++){
