ac41f3bd0dc03ec99fd1a7b784db2f589de93e0f
Prashant Singh <prashantsingh061201@gmail.com>
2022-03-10 02:09:37 +0530

    Time: 33 ms (24.03%), Space: 10.1 MB (26.38%) - LeetHub

---
+++ 16-3sum-closest/16-3sum-closest.cpp
@@ -0,0 +1,33 @@
+class Solution {
+public:
+    int threeSumClosest(vector<int>& nums, int target) {
+        sort(nums.begin(),nums.end());
+        int l,r=nums.size()-1,near=INT_MAX,s=target, ans=0;
+        for(int i=0;i<nums.size();i++){
+            l=i+1,r=nums.size()-1;
+            // s=s-nums[i];
+            while(l<r){
+                // cout<<nums[i]<<" "<<nums[l]<<" "<<nums[r]<<endl;
+                if(nums[l]+nums[r]==s-nums[i]){
+                    return nums[i]+nums[l]+nums[r];
+                    l++;
+                }
+                else if(nums[l]+nums[r]>s-nums[i]){
+                    if(near>abs(nums[l]+nums[r]+nums[i]-target))
+                        near=abs(nums[l]+nums[r]+nums[i]-target) , ans=nums[i]+nums[l]+nums[r] ;
+                    
+                    // near=min(near,abs(nums[l]+nums[r]+nums[i]-target));
+                    r--;
+                }
+                else{
+                    if(near>abs(nums[l]+nums[r]+nums[i]-target))
+                        near=abs(nums[l]+nums[r]+nums[i]-target) , ans=nums[i]+nums[l]+nums[r] ;
+                    // near=min(near,abs(nums[l]+nums[r]+nums[i]-target));
+                    l++;
+                }
+            }
+        }
+        
+        return ans;
+    }
+};
