9b4a7edb05bb0ee8aefb91d0fbb4ee8b1a1bc556
Prashant Singh <prashantsingh061201@gmail.com>
2022-03-15 21:37:01 +0530

    Time: 15 ms (37.72%), Space: 11.6 MB (9.39%) - LeetHub

--- 138-copy-list-with-random-pointer/138-copy-list-with-random-pointer.cpp
+++ 138-copy-list-with-random-pointer/138-copy-list-with-random-pointer.cpp
@@ -19,12 +19,12 @@ public:
     Node* copyRandomList(Node* head) {
         map<Node*, Node*> mp;
         Node* ptr = head;
-        while (ptr!=NULL) {
+        while (ptr) {
             mp[ptr] =new Node(ptr->val);
             ptr = ptr->next;
         }
         ptr = head;
-        while (ptr!=NULL) {
+        while (ptr) {
             mp[ptr]->next = mp[ptr->next];
             mp[ptr]->random = mp[ptr->random];
             ptr = ptr->next;
