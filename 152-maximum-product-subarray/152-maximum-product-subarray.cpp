99e7c16d3c470eb27e67e81e6e35b7d17f0699e2
Prashant Singh <prashantsingh061201@gmail.com>
2022-03-15 14:25:31 +0530

    Time: 15 ms (28.58%), Space: 13.6 MB (95.47%) - LeetHub

--- 152-maximum-product-subarray/152-maximum-product-subarray.cpp
+++ 152-maximum-product-subarray/152-maximum-product-subarray.cpp
@@ -1,17 +1,30 @@
 class Solution {
 public:
     int maxProduct(vector<int>& nums) {
-        long long cur_max=1, cur_min=1, res=nums[0];
-        // for(int i=0;i<nums.size();i++){
-        //     res=max(res,(long long)nums[i]);
-        // }
+//         long long cur_max=1, cur_min=1, res=nums[0];
         
-        for(int i=0;i<nums.size();i++){
-            long long temp= cur_max*nums[i];
-            cur_max=max({cur_max*nums[i], cur_min*nums[i], (long long)nums[i]});
-            cur_min=min({temp, cur_min*nums[i], (long long)nums[i]});
-            res=max(res,cur_max);
+//         for(int i=0;i<nums.size();i++){
+//             long long temp= cur_max*nums[i];
+//             cur_max=max({cur_max*nums[i], cur_min*nums[i], (long long)nums[i]});
+//             cur_min=min({temp, cur_min*nums[i], (long long)nums[i]});
+//             res=max(res,cur_max);
+//         }
+//         return res;
+        
+        //approach 2
+        int mx=nums[0], prod=1, n=nums.size();
+        for(int i=0;i<n;i++){
+            prod*=nums[i];
+            mx=max(prod,mx);
+            if(nums[i]==0) prod=1;
         }
-        return res;
+        prod=1;
+        for(int i=n-1;i>=0;i--){
+            prod*=nums[i];
+            mx=max(mx,prod);
+            if(nums[i]==0) prod=1;
+        }
+        return mx;
+        
     }
 };
