c177476d711059274df3bcf895688d8137058ad9
Prashant Singh <prashantsingh061201@gmail.com>
2022-03-16 18:39:53 +0530

    Time: 11 ms (80.80%), Space: 6.9 MB (90.52%) - LeetHub

--- 3-longest-substring-without-repeating-characters/3-longest-substring-without-repeating-characters.cpp
+++ 3-longest-substring-without-repeating-characters/3-longest-substring-without-repeating-characters.cpp
@@ -4,24 +4,32 @@ public:
         if(s.size()==0) return 0;
         
         int i=0,j=0,mx=0;
-        unordered_set<char> mp;
+//         unordered_set<char> mp;
         
-        while(j<s.size()){
-            if(mp.find(s[j])==mp.end()){
-                mp.insert(s[j]);
-                mx=max(mx, j-i+1);
-                j++;
-            }
-            else{
-                mp.erase(s[i]);
+//         while(j<s.size()){
+//             if(mp.find(s[j])==mp.end()){
+//                 mp.insert(s[j]);
+//                 mx=max(mx, j-i+1);
+//                 j++;
+//             }
+//             else{
+//                 mp.erase(s[i]);
+//                 i++;
+//             }
+//         }
+        
+        int store[256]={0};
+        while(j<s.length())    
+        {
+            store[s[j]]++;      
+            while(store[s[j]]>1)  
+            { 
+                store[s[i]]--;   
                 i++;
             }
+            mx = max(mx,j-i+1);    
+            j++;
         }
-        
-//         map<int,int> mp;
-//         while(j<s.size()){
-            
-//         }
         
         return mx;
     }
