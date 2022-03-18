7b9fcf07957f1129337781c1f1e40b927d215ff9
Prashant Singh <prashantsingh061201@gmail.com>
2022-03-18 22:47:04 +0530

    Time: 40 ms (71.66%), Space: 71.5 MB (11.80%) - LeetHub

--- 2-add-two-numbers/2-add-two-numbers.cpp
+++ 2-add-two-numbers/2-add-two-numbers.cpp
@@ -21,41 +21,14 @@ public:
             int b= (t2!=NULL) ? t2->val: 0;
             sum=car + a + b;
             temp->next= new ListNode(sum%10);
-            // t1->val=sum%10;
-            // t2->val=sum%10;
             car=sum/10;
             temp=temp->next;
-            // temp=t1;
             if(t1!=NULL) t1=t1->next;
             if(t2!=NULL) t2=t2->next;
             
         }
-        // while(t1!=NULL){
-        //     sum=car+ t1->val;
-        //     t1->val=sum%10;
-        //     car=sum/10;
-        //     temp=t1;
-        //     t1=t1->next;
-        // }
-        // while(t2!=NULL){
-        //     sum=car+ t2->val;
-        //     t2->val=sum%10; //t1
-        //     car=sum/10;
-        //     temp=t2;
-        //     t2=t2->next;
-        // }
         if(car!=0)
             temp->next= new ListNode(car);
-        
-//         //to print longer one
-//         t1=l1,t2=l2;
-//         while(t1!=NULL && t2!=NULL){
-//             t1=t1->next;
-//             t2=t2->next;
-//         }        
-        
-//         if(t1==NULL && t2!=NULL)
-//         return l2;
         
         return head->next;
     }
