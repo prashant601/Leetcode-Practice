4b6aee02f5f7e7e0f15f7c57fcb5cc88ab09967f
Prashant Singh <prashantsingh061201@gmail.com>
2022-03-22 02:32:41 +0530

    Time: 4 ms (72.62%), Space: 6.6 MB (64.03%) - LeetHub

--- 763-partition-labels/763-partition-labels.cpp
+++ 763-partition-labels/763-partition-labels.cpp
@@ -1,7 +1,7 @@
 class Solution {
 public:
     vector<int> partitionLabels(string s) {
-        vector<int> mp(26,0);
+        vector<int> mp(26);
         int n=s.size();
         for(int i=0;i<n;i++){
             mp[s[i]-'a']=i;
