640841c7955ef56cb1ae181efe9e2541e023e72a
Prashant Singh <prashantsingh061201@gmail.com>
2022-03-22 02:28:43 +0530

    Time: 8 ms (31.89%), Space: 6.7 MB (43.56%) - LeetHub

---
+++ 763-partition-labels/763-partition-labels.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    vector<int> partitionLabels(string s) {
+        map<char,pair<int,int>>mp;
+        int n=s.size();
+        for(int i=0;i<n;i++){
+            if(mp.find(s[i])==mp.end())
+                mp[s[i]].first=i;
+            mp[s[i]].second=i;
+        }
+        
+        
+        int mx=INT_MIN,pre=0;
+        vector<int> ans;
+        for(int i=0;i<n;i++){
+            mx=max(mx,mp[s[i]].second);
+            if(mx==i){
+                
+                ans.push_back(mx-pre);
+                pre=mx;
+                mx=INT_MIN;
+            }
+        }
+        ans[0]++;
+        return ans;
+    }
+};
