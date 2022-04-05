9ed5a0f2cefe75f645caa3102ffd6c2e0ffaa89a
Prashant Singh <prashantsingh061201@gmail.com>
2022-04-05 23:16:41 +0530

    Time: 602 ms (5.01%), Space: 122.7 MB (31.91%) - LeetHub

--- 234-palindrome-linked-list/234-palindrome-linked-list.cpp
+++ 234-palindrome-linked-list/234-palindrome-linked-list.cpp
@@ -10,22 +10,31 @@
  */
 class Solution {
 private:
-    bool helper(ListNode* head){
-        if(head==NULL) return true;
-        bool a=helper(head->next);
-        if(a==false) return false;
-        else if( head->val!=main->val )
-            return false;
+//     bool helper(ListNode* head){
+//         if(head==NULL) return true;
+//         bool a=helper(head->next);
+//         if(a==false) return false;
+//         else if( head->val!=main->val )
+//             return false;
             
-        else{
-            main=main->next;
-            return true;
-        }
+//         else{
+//             main=main->next;
+//             return true;
+//         }
+//     }
+    bool helper1(ListNode* head, ListNode* &main1){
+        if(head==NULL || main1==NULL) return true;
+        bool a= helper1(head->next,main1);
+        if(a==false) return a;
+        a= (head->val==main1->val);
+        main1=main1->next;
+        return a;
     }
 public:
-    ListNode* main;
+    // ListNode* main;
     bool isPalindrome(ListNode* head) {
-        main=head;
-        return helper(head);
+        // main=head;
+        // return helper(head);
+        return helper1(head,head);
     }
 };
