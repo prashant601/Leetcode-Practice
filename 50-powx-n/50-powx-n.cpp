430338622c9b147bc2a4b6538626760bc005d9bc
Prashant Singh <prashantsingh061201@gmail.com>
2022-04-06 23:11:53 +0530

    Time: 0 ms (100.00%), Space: 5.9 MB (44.91%) - LeetHub

--- 50-powx-n/50-powx-n.cpp
+++ 50-powx-n/50-powx-n.cpp
@@ -1,14 +1,27 @@
 class Solution {
 public:
-    double power(double x, int n) {
-        if(n<=0) return 1;
-        if(x==0) return 0;
-        return x * power(x,n-1);
-    }
+    // double power(double x, int n) {
+    //     if(n<=0) return 1;
+    //     if(x==0) return 0;
+    //     return x * power(x,n-1);
+    // }
     double myPow(double x, int n) {
-        if( (x<1 && x>0 && n>9999999) || (x>1 && n<0 && abs(n)>9999999) ) return 0;
-        if(n<0) return 1/power(x,abs(n));
-        if(x==1 || x==-1) return x;
-        return power(x,n);
+        // if( (x<1 && x>0 && n>9999999) || (x>1 && n<0 && abs(n)>9999999) ) return 0;
+        // if(n<0) return 1/power(x,abs(n));
+        // if(x==1 || x==-1) return x;
+        // return power(x,n);
+        
+        if(n==0)    return 1;
+        if(n<0){
+            x=1/x;
+            n=abs(n);
+        }
+        
+        double res=myPow(x,n/2);
+        
+        if(n%2) res=res*res*x;
+        else    res=res*res;
+        
+        return res; 
     }
 };
