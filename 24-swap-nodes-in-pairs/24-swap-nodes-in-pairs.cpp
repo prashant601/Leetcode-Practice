6c258d8a740f13f8be8f248c1d0a6ad4565956f7
Prashant Singh <prashantsingh061201@gmail.com>
2022-04-08 01:44:02 +0530

    Time: 4 ms (55.55%), Space: 7.6 MB (54.62%) - LeetHub

--- 24-swap-nodes-in-pairs/24-swap-nodes-in-pairs.cpp
+++ 24-swap-nodes-in-pairs/24-swap-nodes-in-pairs.cpp
@@ -11,35 +11,40 @@
 class Solution {
 private:
     
-    void helper(ListNode* head,ListNode* &last){
-        if(head==NULL || head->next==NULL) return;
+//     void helper(ListNode* head,ListNode* &last){
+//         if(head==NULL || head->next==NULL) return;
         
-        ListNode* temp=head->next;
+//         ListNode* temp=head->next;
         
-        if(last!=NULL)
-            last->next=temp;
+//         if(last!=NULL)
+//             last->next=temp;
         
-        if(ans==NULL)
-            ans=temp;
+//         if(ans==NULL)
+//             ans=temp;
+
+//         head->next= temp->next;
+//         temp->next=head;
         
-        // last=;
-            
-        head->next= temp->next;
-        temp->next=head;
-        
-        last=head;
-        helper(head->next,last);
-        
-    }
+//         last=head;
+//         helper(head->next,last);
+//     }
 public:
-    ListNode* ans;
-    ListNode* swapPairs(ListNode* head) {
+    // ListNode* ans;
+    ListNode* swapPairs(ListNode* head) { 
+        // if(head==NULL || head->next==NULL) return head;
+        // ListNode* temp=NULL;
+        // ans=NULL;
+        // helper(head,temp);
+        // return ans;
         
+        //approach 2
         if(head==NULL || head->next==NULL) return head;
+        ListNode *new_head= head->next;
+        ListNode *temp= head->next->next;
+        new_head->next= head;
+        head->next=swapPairs(temp);
         
-        ListNode* temp=NULL;
-        ans=NULL;
-        helper(head,temp);
-        return ans;
+        return new_head;
+        
     }
 };
