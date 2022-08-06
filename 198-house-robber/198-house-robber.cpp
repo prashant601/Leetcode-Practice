ffd38112f0c1765a502b4867d4417e38f3d390d3
Prashant Singh <prashantsingh061201@gmail.com>
2022-08-07 00:00:58 +0530

    Time: 6 ms (17.19%), Space: 7.9 MB (25.02%) - LeetHub

--- 198-house-robber/198-house-robber.cpp
+++ 198-house-robber/198-house-robber.cpp
@@ -1,18 +1,32 @@
 class Solution {
 public:
-    int f(int ind,vector<int>&nums, vector<int>&dp){
-        if(ind==0) return nums[ind];
-        if(ind<0) return 0;
+//     int f(int ind,vector<int>&nums, vector<int>&dp){
+//         if(ind==0) return nums[ind];
+//         if(ind<0) return 0;
         
-        if(dp[ind]!=-1) return dp[ind];
+//         if(dp[ind]!=-1) return dp[ind];
         
-        int pick= nums[ind] + f(ind-2, nums,dp);
-        int  notpick = 0 + f(ind-1, nums,dp);
-        return dp[ind]=max(pick, notpick);
-    }
+//         int pick= nums[ind] + f(ind-2, nums,dp);
+//         int  notpick = 0 + f(ind-1, nums,dp);
+//         return dp[ind]=max(pick, notpick);
+//     }
     int rob(vector<int>& nums) {
+        // int n=nums.size();
+        // vector<int> dp(n,-1);
+        // return f(n-1,nums,dp);
+        
+        
+        //tabular
         int n=nums.size();
-        vector<int> dp(n,-1);
-        return f(n-1,nums,dp);
+        vector<int> dp(n,0);
+        dp[0]= nums[0];
+        for(int i=1;i<n;i++){
+            int p= nums[i];
+            if(i>1)
+            p+=dp[i-2];
+            int np= 0 + dp[i-1];
+            dp[i]= max(p,np);
+        }
+        return dp[n-1];
     }
 };
