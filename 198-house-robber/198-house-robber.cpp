ac47815d6ef5886d6dd465ff11606318099abfef
Prashant Singh <prashantsingh061201@gmail.com>
2022-08-07 00:05:19 +0530

    Time: 3 ms (54.99%), Space: 7.6 MB (76.61%) - LeetHub

--- 198-house-robber/198-house-robber.cpp
+++ 198-house-robber/198-house-robber.cpp
@@ -17,16 +17,31 @@ public:
         
         
         //tabular
+        // int n=nums.size();
+        // vector<int> dp(n,0);
+        // dp[0]= nums[0];
+        // for(int i=1;i<n;i++){
+        //     int p= nums[i];
+        //     if(i>1)
+        //     p+=dp[i-2];
+        //     int np= 0 + dp[i-1];
+        //     dp[i]= max(p,np);
+        // }
+        // return dp[n-1];
+        
+        //space opt. tabular
         int n=nums.size();
-        vector<int> dp(n,0);
-        dp[0]= nums[0];
+        int prev=nums[0],prev2=0,curr;
         for(int i=1;i<n;i++){
             int p= nums[i];
             if(i>1)
-            p+=dp[i-2];
-            int np= 0 + dp[i-1];
-            dp[i]= max(p,np);
+            p+=prev2;
+            int np= 0 + prev;
+            curr= max(p,np);
+            prev2=prev;
+            prev=curr;
         }
-        return dp[n-1];
+        return prev;
+        
     }
 };
