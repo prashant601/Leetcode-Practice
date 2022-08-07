c113caf6d3ef1204d0c344f7d1e1db299587aa46
Prashant Singh <prashantsingh061201@gmail.com>
2022-08-07 15:54:37 +0530

    Time: 6 ms (12.59%), Space: 6.2 MB (55.22%) - LeetHub

--- 62-unique-paths/62-unique-paths.cpp
+++ 62-unique-paths/62-unique-paths.cpp
@@ -15,18 +15,35 @@ public:
         // return f(m-1,n-1,dp);
         
         //tabular dp
-        vector<vector<int>> dp(m+1, vector<int>(n+1,0));
-        dp[0][0]=1;
+//         vector<vector<int>> dp(m+1, vector<int>(n+1,0));
+//         dp[0][0]=1;
         
+//         for(int i=0;i<m;i++){
+//             for(int j=0;j<n;j++){
+//                 if(i==0 && j==0) continue;
+//                 int up= 0, left=0;  //else part
+//                 if(i>0) up= dp[i-1][j];
+//                 if(j>0) left=dp[i][j-1];
+//                 dp[i][j]= up+left;
+//             }
+//         }
+//         return dp[m-1][n-1];
+                
+        //space opt. tabular
+        vector<int> prev(n,0);
         for(int i=0;i<m;i++){
+            vector<int> cur(n,0);
             for(int j=0;j<n;j++){
-                if(i==0 && j==0) continue;
-                int up= 0, left=0;
-                if(i>0) up= dp[i-1][j];
-                if(j>0) left=dp[i][j-1];
-                dp[i][j]= up+left;
+                if(i==0 && j==0) cur[j]=1;
+                else{
+                    int up= 0, left=0;
+                    if(i>0) up= prev[j];
+                    if(j>0) left=cur[j-1];
+                    cur[j]= up+left;
+                }
             }
+            prev=cur;
         }
-        return dp[m-1][n-1];
+        return prev[n-1];
     }
 };
