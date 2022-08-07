11a8fef6414af6bf6f5b0155d81381d0cbe6cbb6
Prashant Singh <prashantsingh061201@gmail.com>
2022-08-07 18:45:51 +0530

    Time: 9 ms (58.39%), Space: 9 MB (37.91%) - LeetHub

--- 120-triangle/120-triangle.cpp
+++ 120-triangle/120-triangle.cpp
@@ -18,20 +18,40 @@ public:
         // return f(0, 0, n, m, triangle, dp);
         
         //tabular 
-        int n=triangle.size();
-        int m= triangle[n-1].size();
-        vector<vector<int>> dp(n, vector<int>(m,0));
+//         int n=triangle.size();
+//         int m= triangle[n-1].size();
+//         vector<vector<int>> dp(n, vector<int>(m,0));
         
-        for(int j=0;j<n;j++) dp[n-1][j] = triangle[n-1][j];  // initalization as in base case
+//         for(int j=0;j<n;j++) dp[n-1][j] = triangle[n-1][j];  // initalization as in base case
+        
+//         for(int i=n-2;i>=0;i--){
+//             for(int j=i;j>=0;j--){
+//                 int down= triangle[i][j] + dp[i+1][j];
+//                 int downright= triangle[i][j] + dp[i+1][j+1];
+//                 dp[i][j]= min(down, downright);
+//             }
+//         }
+        
+//         return dp[0][0];
+        
+        // space optimized tabular
+        int n=triangle.size();
+        vector<int> prev(n,0);
+        
+        for(int j=0;j<n;j++) prev[j] = triangle[n-1][j];  // initalization as in base case
         
         for(int i=n-2;i>=0;i--){
+            
+            vector<int> cur(n,0);
+            
             for(int j=i;j>=0;j--){
-                int down= triangle[i][j] + dp[i+1][j];
-                int downright= triangle[i][j] + dp[i+1][j+1];
-                dp[i][j]= min(down, downright);
+                int down= triangle[i][j] + prev[j];
+                int downright= triangle[i][j] + prev[j+1];
+                cur[j]= min(down, downright);
             }
+            prev= cur;
         }
         
-        return dp[0][0];
+        return prev[0];
     }
 };
