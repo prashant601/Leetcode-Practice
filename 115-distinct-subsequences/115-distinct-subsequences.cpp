f479304ef237dd1e8374d2a567c4e7b9474cfef3
Prashant Singh <prashantsingh061201@gmail.com>
2022-08-10 00:57:09 +0530

    Time: 41 ms (79.11%), Space: 6.5 MB (89.30%) - LeetHub

--- 115-distinct-subsequences/115-distinct-subsequences.cpp
+++ 115-distinct-subsequences/115-distinct-subsequences.cpp
@@ -29,18 +29,33 @@ public:
 //         }
 //         return (int)dp[n][m];
         
-        //space optimized tabular
-        vector<double> prev(m+1,0),curr(m+1,0);
-        prev[0]=curr[0]=1;
+        
+        
+//         //space optimized tabular
+//         vector<double> prev(m+1,0),curr(m+1,0);
+//         prev[0]=curr[0]=1;
+        
+//         for(int i=1;i<=n;i++){
+//             for(int j=1;j<=m;j++){
+//                 if(s[i-1]==t[j-1])
+//                     curr[j]=prev[j-1]+prev[j];
+//                 else
+//                     curr[j]=prev[j];
+//             }
+//             prev=curr;
+//         }
+//         return prev[m];
+        
+        
+        //cool 
+        vector<double> prev(m+1,0);
+        prev[0]=1;
         
         for(int i=1;i<=n;i++){
-            for(int j=1;j<=m;j++){
+            for(int j=m;j>=1;j--){
                 if(s[i-1]==t[j-1])
-                    curr[j]=prev[j-1]+prev[j];
-                else
-                    curr[j]=prev[j];
+                    prev[j]=prev[j-1]+prev[j];
             }
-            prev=curr;
         }
         return prev[m];
     }
