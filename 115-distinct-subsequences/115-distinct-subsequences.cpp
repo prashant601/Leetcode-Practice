0404b07eb3453e45e69cac5aee3c42e764c14c43
Prashant Singh <prashantsingh061201@gmail.com>
2022-08-10 00:50:02 +0530

    Time: 46 ms (72.29%), Space: 6.5 MB (89.30%) - LeetHub

--- 115-distinct-subsequences/115-distinct-subsequences.cpp
+++ 115-distinct-subsequences/115-distinct-subsequences.cpp
@@ -15,18 +15,33 @@ public:
         // return f(n-1,m-1,s,t,dp);
         
         //tabular
-        vector<vector<double>> dp(n+1, vector<double>(m+1,0));
-        for(int i=0;i<=n;i++) dp[i][0]=1;
-        // for(int j=0;j<=m;j++) dp[0][j]=0;// no need
+//         vector<vector<double>> dp(n+1, vector<double>(m+1,0));
+//         for(int i=0;i<=n;i++) dp[i][0]=1;
+//         // for(int j=0;j<=m;j++) dp[0][j]=0;// no need
+        
+//         for(int i=1;i<=n;i++){
+//             for(int j=1;j<=m;j++){
+//                 if(s[i-1]==t[j-1])
+//                     dp[i][j]=dp[i-1][j-1]+dp[i-1][j];
+//                 else
+//                     dp[i][j]=dp[i-1][j];
+//             }
+//         }
+//         return (int)dp[n][m];
+        
+        //space optimized tabular
+        vector<double> prev(m+1,0),curr(m+1,0);
+        prev[0]=curr[0]=1;
         
         for(int i=1;i<=n;i++){
             for(int j=1;j<=m;j++){
                 if(s[i-1]==t[j-1])
-                    dp[i][j]=dp[i-1][j-1]+dp[i-1][j];
+                    curr[j]=prev[j-1]+prev[j];
                 else
-                    dp[i][j]=dp[i-1][j];
+                    curr[j]=prev[j];
             }
+            prev=curr;
         }
-        return (int)dp[n][m];
+        return prev[m];
     }
 };
