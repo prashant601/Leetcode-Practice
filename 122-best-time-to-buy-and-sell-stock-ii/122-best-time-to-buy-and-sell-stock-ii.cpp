36d1c091e90d6c172622742aa8b0b89f459e8ea1
Prashant Singh <prashantsingh061201@gmail.com>
2022-08-10 15:50:35 +0530

    Time: 31 ms (5.28%), Space: 15.3 MB (13.82%) - LeetHub

--- 122-best-time-to-buy-and-sell-stock-ii/122-best-time-to-buy-and-sell-stock-ii.cpp
+++ 122-best-time-to-buy-and-sell-stock-ii/122-best-time-to-buy-and-sell-stock-ii.cpp
@@ -1,24 +1,43 @@
 class Solution {
 public:
-    int f(int ind, int buy, vector<int>&prices,vector<vector<int>>&dp){
-        if(ind==prices.size()) return 0;
+//     int f(int ind, int buy, vector<int>&prices,vector<vector<int>>&dp){
+//         if(ind==prices.size()) return 0;
         
-        if(dp[ind][buy]!=-1) return dp[ind][buy];
+//         if(dp[ind][buy]!=-1) return dp[ind][buy];
         
-        if(buy==1){    // take nottake are profits
-            int take= f(ind+1,0,prices,dp) - prices[ind];   //buys is set to 0 , i.e we can only sell
-            int nottake= 0+ f(ind+1,1,prices,dp);
-            return dp[ind][buy]= max(take, nottake);
-        }
-        else{
-            int sell= prices[ind]+ f(ind+1,1,prices,dp); // now we can buy again
-            int notsell= 0+ f(ind+1,0,prices,dp); // cannot buy, first sell this
-            return dp[ind][buy]= max(sell, notsell);
-        }
-    }
+//         if(buy==1){    // take nottake are profits
+//             int take= f(ind+1,0,prices,dp) - prices[ind];   //buys is set to 0 , i.e we can only sell
+//             int nottake= 0+ f(ind+1,1,prices,dp);
+//             return dp[ind][buy]= max(take, nottake);
+//         }
+//         else{
+//             int sell= prices[ind]+ f(ind+1,1,prices,dp); // now we can buy again
+//             int notsell= 0+ f(ind+1,0,prices,dp); // cannot buy, first sell this
+//             return dp[ind][buy]= max(sell, notsell);
+//         }
+//     }
     int maxProfit(vector<int>& prices) {
+        // int n= prices.size();
+        // vector<vector<int>> dp(n+1, vector<int>(3,-1));  // buys is only 0 1 so dp is of n*2
+        // return f(0,1, prices,dp); // 1 is for buy   
+        
+        // tabular
         int n= prices.size();
-        vector<vector<int>> dp(n+1, vector<int>(3,-1));  // buys is only 0 1 so dp is of n*2
-        return f(0,1, prices,dp); // 1 is for buy   
+        vector<vector<int>> dp(n+1, vector<int>(2,0));
+        for(int i=n-1;i>=0;i--){
+            for(int j=0;j<2;j++){
+                if(j==1){
+                    int take= dp[i+1][0] - prices[i];   //buys is set to 0 , i.e we can only sell
+                    int nottake= 0+ dp[i+1][1];
+                    dp[i][j]= max(take, nottake);   
+                }
+                else{
+                    int sell= prices[i]+ dp[i+1][1]; // now we can buy again
+                    int notsell= 0+ dp[i+1][0]; // cannot buy, first sell this
+                    dp[i][j]= max(sell, notsell);
+                }
+            }
+        }
+        return dp[0][1];
     }
 };
