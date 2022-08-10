ab7f0869b700ab11c9a2d39a04aa056bdacdbece
Prashant Singh <prashantsingh061201@gmail.com>
2022-08-10 15:55:08 +0530

    Time: 24 ms (10.56%), Space: 13 MB (57.64%) - LeetHub

--- 122-best-time-to-buy-and-sell-stock-ii/122-best-time-to-buy-and-sell-stock-ii.cpp
+++ 122-best-time-to-buy-and-sell-stock-ii/122-best-time-to-buy-and-sell-stock-ii.cpp
@@ -21,23 +21,43 @@ public:
         // vector<vector<int>> dp(n+1, vector<int>(3,-1));  // buys is only 0 1 so dp is of n*2
         // return f(0,1, prices,dp); // 1 is for buy   
         
-        // tabular
+        // // tabular
+        // int n= prices.size();
+        // vector<vector<int>> dp(n+1, vector<int>(2,0));
+        // for(int i=n-1;i>=0;i--){
+        //     for(int j=0;j<2;j++){
+        //         if(j==1){
+        //             int take= dp[i+1][0] - prices[i];   //buys is set to 0 , i.e we can only sell
+        //             int nottake= 0+ dp[i+1][1];
+        //             dp[i][j]= max(take, nottake);   
+        //         }
+        //         else{
+        //             int sell= prices[i]+ dp[i+1][1]; // now we can buy again
+        //             int notsell= 0+ dp[i+1][0]; // cannot buy, first sell this
+        //             dp[i][j]= max(sell, notsell);
+        //         }
+        //     }
+        // }
+        // return dp[0][1];
+        
+        //tabular dp space optimised
         int n= prices.size();
-        vector<vector<int>> dp(n+1, vector<int>(2,0));
+        vector<int> front(2,0), curr(2,0); // we can use 4 variables
         for(int i=n-1;i>=0;i--){
             for(int j=0;j<2;j++){
                 if(j==1){
-                    int take= dp[i+1][0] - prices[i];   //buys is set to 0 , i.e we can only sell
-                    int nottake= 0+ dp[i+1][1];
-                    dp[i][j]= max(take, nottake);   
+                    int take= front[0] - prices[i];   //buys is set to 0 , i.e we can only sell
+                    int nottake= 0+ front[1];
+                    curr[j]= max(take, nottake);   
                 }
                 else{
-                    int sell= prices[i]+ dp[i+1][1]; // now we can buy again
-                    int notsell= 0+ dp[i+1][0]; // cannot buy, first sell this
-                    dp[i][j]= max(sell, notsell);
+                    int sell= prices[i]+ front[1]; // now we can buy again
+                    int notsell= 0+ front[0]; // cannot buy, first sell this
+                    curr[j]= max(sell, notsell);
                 }
             }
+            front=curr;
         }
-        return dp[0][1];
+        return front[1];
     }
 };
