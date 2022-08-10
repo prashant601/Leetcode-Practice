bc2bcc18a0754ddc6828d3a5a21f702f76f3c722
Prashant Singh <prashantsingh061201@gmail.com>
2022-08-10 18:01:52 +0530

    Time: 0 ms (100.00%), Space: 12.1 MB (24.13%) - LeetHub

---
+++ 309-best-time-to-buy-and-sell-stock-with-cooldown/309-best-time-to-buy-and-sell-stock-with-cooldown.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    //buy sell stock II
+    int f(int ind, int buy, vector<int>&prices,vector<vector<int>>&dp){
+        if(ind>=prices.size()) return 0;
+        
+        if(dp[ind][buy]!=-1) return dp[ind][buy];
+        
+        if(buy==1){    // take nottake are profits
+            int take= f(ind+1,0,prices,dp) - prices[ind];   //buys is set to 0 , i.e we can only sell
+            int nottake= 0+ f(ind+1,1,prices,dp);
+            return dp[ind][buy]= max(take, nottake);
+        }
+        else{
+            int sell= prices[ind]+ f(ind+1+1,1,prices,dp); // now we can buy again , + 1 is for cooldown 
+            int notsell= 0+ f(ind+1,0,prices,dp); // cannot buy, first sell this
+            return dp[ind][buy]= max(sell, notsell);
+        }
+    }
+    int maxProfit(vector<int>& prices) {
+        int n= prices.size();
+        vector<vector<int>> dp(n+1, vector<int>(3,-1));  // buys is only 0 1 so dp is of n*2
+        return f(0,1, prices,dp); // 1 is for buy  
+    }
+};
