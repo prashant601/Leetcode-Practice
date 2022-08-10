083f123ec0a6f468218d92113feaf05e0a0862a3
Prashant Singh <prashantsingh061201@gmail.com>
2022-08-10 15:59:46 +0530

    Time: 19 ms (20.16%), Space: 13.1 MB (57.64%) - LeetHub

--- 122-best-time-to-buy-and-sell-stock-ii/122-best-time-to-buy-and-sell-stock-ii.cpp
+++ 122-best-time-to-buy-and-sell-stock-ii/122-best-time-to-buy-and-sell-stock-ii.cpp
@@ -61,3 +61,17 @@ public:
         return front[1];
     }
 };
+
+
+//best solution
+// class Solution {           
+// public:
+//     int maxProfit(vector<int>& prices) {
+//         int p=0;
+//         for(int i=0;i<prices.size()-1;i++){
+//             if(prices[i]<prices[i+1])
+//                 p+=prices[i+1]-prices[i];
+//         }
+//         return p;
+//     }
+// };
