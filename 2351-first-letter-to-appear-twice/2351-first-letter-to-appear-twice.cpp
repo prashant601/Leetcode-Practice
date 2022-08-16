7484f600f171aed21d60f780db01036fbf1cd995
Prashant Singh <prashantsingh061201@gmail.com>
2022-08-17 01:11:03 +0530

    Time: 2 ms (46.60%), Space: 6.3 MB (18.83%) - LeetHub

---
+++ 2351-first-letter-to-appear-twice/2351-first-letter-to-appear-twice.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    char repeatedCharacter(string s) {
+        
+        unordered_map<char,int>mp;
+        char ans;
+        for(auto x:s){
+            mp[x]++;
+            if(mp[x]==2)
+            {
+                ans=x; return x;
+            }
+        }
+        return ans;
+    }
+};
