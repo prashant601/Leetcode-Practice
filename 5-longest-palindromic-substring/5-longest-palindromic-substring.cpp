7efa8aeb53a7904ee22fd997fcaa15b4fb111c88
Prashant Singh <prashantsingh061201@gmail.com>
2022-09-12 14:35:11 +0530

    Time: 33 ms (72.37%), Space: 6.9 MB (83.61%) - LeetHub

--- 5-longest-palindromic-substring/5-longest-palindromic-substring.cpp
+++ 5-longest-palindromic-substring/5-longest-palindromic-substring.cpp
@@ -1,32 +1,34 @@
 class Solution {
 public:
+    int mx=1, st=0,end=0;
+    void getMax(int l, int r, string& s){
+        int n=s.size();
+        while(l>=0 and r<n){
+            if(s[l]==s[r])
+                l--,r++;
+            else 
+                break;
+            
+            int len= r-l-1;
+            if(mx<len){
+                mx=len;
+                st= l+1;
+                end= r-1;
+            }
+        }
+    }
+    
     string longestPalindrome(string s) {
         int n= s.size();
-        int a=0,b=0;
-        vector<vector<int>> dp(n+1,vector<int> (n+1,0));
-        int mx=1;
-        for(int i=0;i<n;i++){
-           dp[i][i]=1;
-        }
+        
         for(int i=0;i<n-1;i++){
-           dp[i][i+1]= (s[i]==s[i+1])? 1: 0 ;
-            if(dp[i][i+1]==1){
-                mx=2;
-                a=i;
-                b=i+1;
-            }
-        
+            getMax(i,i,s);     
         }
         
-        for(int len=3;len<=n;len++){
-            for(int i=0;i+len<=n;i++){
-                dp[i][i+len-1]= (s[i]==s[i+len-1] and dp[i+1][i+len-2]) ? 1 : 0;
-                if(dp[i][i+len-1]==1 and mx< len){
-                    a= i,b= i+len-1;
-                    mx=max(mx, len);
-                }
-            }
+        for(int i=0;i<n-1;i++){
+            getMax(i,i+1,s);
         }
-        return s.substr(a, b-a+1);
+        
+        return s.substr(st,mx);
     }
 };
