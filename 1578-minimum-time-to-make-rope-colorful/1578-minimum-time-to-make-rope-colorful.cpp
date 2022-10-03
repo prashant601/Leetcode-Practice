99f45a0220c47dca19ab7d38d6469faac410e917
Prashant Singh <prashantsingh061201@gmail.com>
2022-10-03 12:37:58 +0530

    Time: 780 ms (5.08%), Space: 137.7 MB (5.02%) - LeetHub

---
+++ 1578-minimum-time-to-make-rope-colorful/1578-minimum-time-to-make-rope-colorful.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    int minCost(string col, vector<int>& time) {
+        int n= col.size(), ans=0;
+        for(int i=0;i<n;i++){
+            vector<int> l;
+            l.push_back(0);
+            while(i<n and col[i]==col[i+1]){
+                l.push_back(time[i]);
+                i++;
+            }
+            l.push_back(time[i]);
+            
+            // cout<<"cycle i"<<" ";
+            // for(auto i: l) cout<<i<<" ";
+            // cout<<"end cycle"<<endl;
+            
+            sort(l.begin(), l.end());
+            for(int i=0;i<l.size()-1;i++){
+                ans+= l[i];
+                // cout<<"this is added "<<l[i]<<" ";
+            }
+        }
+        return ans;
+    }
+};
