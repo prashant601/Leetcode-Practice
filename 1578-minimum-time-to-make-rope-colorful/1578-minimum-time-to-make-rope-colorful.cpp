c35e16a9e7b9f74860edb2728706496497ea2ad5
Prashant Singh <prashantsingh061201@gmail.com>
2022-10-03 14:08:13 +0530

    Time: 495 ms (6.85%), Space: 95.5 MB (50.05%) - LeetHub

--- 1578-minimum-time-to-make-rope-colorful/1578-minimum-time-to-make-rope-colorful.cpp
+++ 1578-minimum-time-to-make-rope-colorful/1578-minimum-time-to-make-rope-colorful.cpp
@@ -1,28 +1,43 @@
 class Solution {
 public:
     int minCost(string col, vector<int>& time) {
-        int n= col.size(), ans=0;
-        int i=0;
-        while(i<n){
-            vector<int> l;
-            l.push_back(0);
-            while(i<n and col[i]==col[i+1]){
-                l.push_back(time[i]);
-                i++;
-            }
-            l.push_back(time[i]);
+//         int n= col.size(), ans=0;
+//         int i=0;
+//         while(i<n){
+//             vector<int> l;
+//             l.push_back(0);
+//             while(i<n and col[i]==col[i+1]){
+//                 l.push_back(time[i]);
+//                 i++;
+//             }
+//             l.push_back(time[i]);
             
-            // cout<<"cycle i"<<" ";
-            // for(auto i: l) cout<<i<<" ";
-            // cout<<"end cycle"<<endl;
+//             // cout<<"cycle i"<<" ";
+//             // for(auto i: l) cout<<i<<" ";
+//             // cout<<"end cycle"<<endl;
             
-            sort(l.begin(), l.end());
-            for(int i=0;i<l.size()-1;i++){
-                ans+= l[i];
-                // cout<<"this is added "<<l[i]<<" ";
+//             sort(l.begin(), l.end());
+//             for(int i=0;i<l.size()-1;i++){
+//                 ans+= l[i];
+//                 // cout<<"this is added "<<l[i]<<" ";
+//             }
+//             i++;
+//         }
+//         return ans;
+        
+        
+        // For a group of continuous same characters, we need to delete all the group but leave only one character.
+        //    cost = sum_cost(group) - max_cost(group)
+        int res = 0, max_cost = 0, sum_cost = 0, n = col.size();
+        for (int i = 0; i < n; ++i) {
+            if (i > 0 && col[i] != col[i - 1]) {
+                res += sum_cost - max_cost;
+                sum_cost = max_cost = 0;
             }
-            i++;
+            sum_cost += time[i];
+            max_cost = max(max_cost, time[i]);
         }
-        return ans;
+        res += sum_cost - max_cost;
+        return res;
     }
 };
